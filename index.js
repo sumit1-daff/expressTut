63472ec07e2d59f01bc36a7e03ad44f39bc81d73
sumit singh <sumit.singh1@daffodilsw.com>
2024-09-26 11:50:35 +0530

    add middleware

--- index.js
+++ index.js
@@ -2,38 +2,61 @@ const express = require("express");
 
 const fs = require("fs");
 
-const data = JSON.parse(fs.readFileSync("data.json",'utf-8'));
-const product  = data.product;
-const index = fs.readFileSync("index.html",'utf-8');
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const product = data.product;
+const index = fs.readFileSync("index.html", "utf-8");
 
 const server = express();
 
+/*******************************MIDDLEWARE******************************************/
+server.use(express.json()); //body parser
+server.use(express.urlencoded()) // url parser jaisa kuch hai!
 
+server.use(express.static("public"));
+server.use((req, res, next) => {
+  console.log(
+    req.method,
+    req.ip,
+    req.hostname,
+    req.get("User-agent"),
+    new Date()
+  );
+  next();
+});
+
+const auth = (req, res, next) => {
+  console.log(req.body);
+  if (req.body.password == "234") {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+};
 
 /*********API*********ENDPOINT*************ROUTE*********************** */
-server.get('/',(req,res)=>{
-    // res.sendStatus(404);
-    // res.json(product);
-    // res.status(201).send("Sumit Singh");
-    res.json({"type" : "GET"});
-})
+server.get("/", auth, (req, res) => {
+  // res.sendStatus(404);
+  // res.json(product);
+  // res.status(201).send("Sumit Singh");
+  res.json({ type: "GET" });
+});
 
-server.post('/',(req,res)=>{
-    res.json({"type" : "POST"});
-})
+server.post("/", (req, res) => {
+  res.json({ type: "POST" });
+});
 
-server.delete('/',(req,res)=>{
-    res.json({"type" : "DELETE"});
-})
+server.delete("/", (req, res) => {
+  res.json({ type: "DELETE" });
+});
 
-server.patch('/',(req,res)=>{
-    res.json({"type" : "PATCH"});
-})
+server.patch("/", (req, res) => {
+  res.json({ type: "PATCH" });
+});
 
-server.put('/',(req,res)=>{
-    res.json({"type" : "PUT"});
-})
+server.put("/", (req, res) => {
+  res.json({ type: "PUT" });
+});
 
-server.listen(8080,() =>{
-    console.log("Server Started");
+server.listen(8080, () => {
+  console.log("Server Started");
 });
