c0dceb926562e0fb863e072ecd9492fd653e73c7
sumit singh <sumit.singh1@daffodilsw.com>
2024-09-26 13:27:48 +0530

    rest apis created

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const fs = require("fs");
 
 const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-const product = data.product;
+const users = data.users;
 const index = fs.readFileSync("index.html", "utf-8");
 
 const server = express();
@@ -12,9 +12,9 @@ const server = express();
 server.use(express.json()); //body parser
 // server.use(express.urlencoded()) // url parser jaisa kuch hai!
 
-server.use(morgan('default'));
+// server.use(morgan('default'));
 server.use(express.static("public"));
-// server.use((req, res, next) => {
+// server.use((req, res, next) => { //application middleware which covers whole application
 //   console.log(
 //     req.method,
 //     req.ip,
@@ -27,31 +27,71 @@ server.use(express.static("public"));
 
 
 
-const auth = (req, res, next) => {
-//   console.log(req.body);
-//   if (req.body.password == "234") {
-//     next();
-//   } else {
-//     res.sendStatus(401);
-//   }
-next();
+const auth = (req, res, next) => { //Route Middle ware which can be passed as a  parameter to the function during routing
+  console.log(req.body);
+  if (req.body.password == "234") {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
 };
 
 /*********API*********ENDPOINT*************ROUTE*********************** */
-server.get("/user/:id", auth, (req, res) => {
-    console.log(req.params); // it reads the parametes passed in the url.
-    res.json({type : "GET"});
-  });
-server.get("/", auth, (req, res) => {
+
+ //POST API - CREATE API
+ server.post("/users", (req, res) => {
+    console.log(req.body);
+    users.push(req.body);
+  res.json(req.body);
+});
+
+
+//GET USERS - READ API
+server.get("/", (req, res) => {
   // res.sendStatus(404);
   // res.json(product);
   // res.status(201).send("Sumit Singh");
   res.json({ type: "GET" });
 });
 
-server.post("/", (req, res) => {
-  res.json({ type: "POST" });
-});
+server.get("/users",(req,res)=>{
+    res.json(users);
+})
+server.get("/users/:id",(req, res) => {
+    //console.log(req.params); // it reads the parametes passed in the url.
+    const id = +req.params.id;
+    const user = users.find(u=>u.id===id);
+    res.json(user);
+  });
+
+  //UPDATE API - PUT API
+  server.put("/users/:id",(req, res) => {
+    //console.log(req.params); // it reads the parametes passed in the url.
+    const id = +req.params.id;
+    const userIndex = users.findIndex(u=>u.id===id);
+    users.splice(userIndex,1,{...req.body, id:id})
+    res.status(201).json({"name": "shuham"});
+  });
+
+  //UPDATE API - PATCH API
+  server.patch("/users/:id",(req, res) => {
+    //console.log(req.params); // it reads the parametes passed in the url.
+    const id = +req.params.id;
+    const userIndex = users.findIndex(u=>u.id===id);
+    users.splice(userIndex,1,{...users[userIndex],...req.body})
+    res.status(201).json();
+  });
+
+  //DELETE API - DELETE API
+  server.delete("/users/:id",(req, res) => {
+    //console.log(req.params); // it reads the parametes passed in the url.
+    const id = +req.params.id;
+    const userIndex = users.findIndex(u=>u.id===id);
+    const user = users[userIndex];
+    users.splice(userIndex,1);
+    res.status(201).json(user);
+  });
+
 
 server.delete("/", (req, res) => {
   res.json({ type: "DELETE" });
@@ -59,10 +99,6 @@ server.delete("/", (req, res) => {
 
 server.patch("/", (req, res) => {
   res.json({ type: "PATCH" });
-});
-
-server.put("/", (req, res) => {
-  res.json({ type: "PUT" });
 });
 
 server.listen(8080, () => {
