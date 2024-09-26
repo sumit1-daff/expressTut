79d2b002a99b233b2ad3af9ecfd6a6cbe3efd578
sumit singh <sumit.singh1@daffodilsw.com>
2024-09-26 10:59:15 +0530

    add api endpoint

--- index.js
+++ index.js
@@ -3,4 +3,37 @@ const express = require("express");
 const fs = require("fs");
 
 const data = JSON.parse(fs.readFileSync("data.json",'utf-8'));
-const index = fs.readFileSync("index.html",utf-8);
+const product  = data.product;
+const index = fs.readFileSync("index.html",'utf-8');
+
+const server = express();
+
+
+
+/*********API*********ENDPOINT*************ROUTE*********************** */
+server.get('/',(req,res)=>{
+    // res.sendStatus(404);
+    // res.json(product);
+    // res.status(201).send("Sumit Singh");
+    res.json({"type" : "GET"});
+})
+
+server.post('/',(req,res)=>{
+    res.json({"type" : "POST"});
+})
+
+server.delete('/',(req,res)=>{
+    res.json({"type" : "DELETE"});
+})
+
+server.patch('/',(req,res)=>{
+    res.json({"type" : "PATCH"});
+})
+
+server.put('/',(req,res)=>{
+    res.json({"type" : "PUT"});
+})
+
+server.listen(8080,() =>{
+    console.log("Server Started");
+});
